6868c97fec608b03c98f03c5ddb6cfc35a9eca77
Aljaž <ar1961@student.uni-lj.si>
2018-07-19 11:13:36 +0200

    dobavnica + narocilnica + dodan potnik

--- narocila/views.py
+++ narocila/views.py
@@ -70,7 +70,7 @@ def stara_narocila(request):
     if request.method == 'POST' and 'prenesi_dobavnica' in request.POST:
 
         # dobavnica ID!
-        #list vseh imen skupine izdelkov + filas, ce je nakonc ksn nic ga ne das na dobavnico. Hash table,dictionary
+        
 
         narocilo_id = request.POST['narocilo_id']
         narocilo = Narocilo.objects.get(id = narocilo_id)
@@ -82,7 +82,7 @@ def stara_narocila(request):
             rightMargin=20,leftMargin=20,
             topMargin=20,bottomMargin=20)
 
-        story = natisni_narocilnica(request,narocilo)
+        story = natisni_dobavnica(request,narocilo)
         doc.build(story)
         return response
     if request.method == 'POST' and 'uveljavi_stNarocila' in request.POST:
@@ -102,31 +102,39 @@ def stara_narocila(request):
     return render(request,'narocila/stara_narocila.html',context)
 
 
-def natisni_dobavnica(request,narocilo):
+def natisni_dobavnica(request, narocilo):
     narocila_izdelka = narocilo.narocila_izdelka.all()
     #prestejes za skupino izdelkov!!
+    #list vseh imen skupine izdelkov + filas, ce je nakonc ksn nic ga ne das na dobavnico. Hash table,dictionary
+    skupineIzdelkov = SkupinaIzdelkov.objects.all()
+    
+    tabela = dict((skupina,0) for skupina in skupineIzdelkov)
+    
+    for narocilo_izdelka in narocila_izdelka:
+        tabela[narocilo_izdelka.izdelek.skupina_izdelkov] += narocilo_izdelka.kolicina
+    
 
-
-    uporabnik = Uporabnik.objects.get(user = request.user)
+    uporabnik = narocilo.uporabnik
     story = []
 
     formatted_time = datetime.date.today()
     formatted_time = str(formatted_time)
-    tabela = formatted_time.split("-")
-    formatted_time = tabela[2] + "." + tabela[1] + "." + tabela[0]
+    tabela1 = formatted_time.split("-")
+    formatted_time = tabela1[2] + "." + tabela1[1] + "." + tabela1[0]
 
             
     styles=getSampleStyleSheet()
     p0 = ParagraphStyle('MyNormal',parent=styles['Normal'], alignment=TA_RIGHT)
-    p1 = styles.add(ParagraphStyle(name='Center', alignment=TA_CENTER))
-    p2 = styles.add(ParagraphStyle(name='Right', alignment=TA_RIGHT))
-    p3 = styles.add(ParagraphStyle(name='Left', alignment=TA_LEFT))
-    p4 = styles.add(ParagraphStyle(name='Line_Data', alignment=TA_LEFT, fontSize=11, leading=10))
-    p5 = styles.add(ParagraphStyle(name='Line_Data_Small', alignment=TA_LEFT, fontSize=7, leading=8))
-    p6 = styles.add(ParagraphStyle(name='Line_Data_Large', alignment=TA_LEFT, fontSize=12, leading=12))
-    p7 = styles.add(ParagraphStyle(name='Line_Data_Largest', alignment=TA_LEFT, fontSize=14, leading=15))
-    p8 = styles.add(ParagraphStyle(name='Line_Label', font='Helvetica-Bold', fontSize=7, leading=6, alignment=TA_LEFT))
-    p9 = styles.add(ParagraphStyle(name='Line_Label_Center', font='Helvetica-Bold', fontSize=7, alignment=TA_CENTER))
+    p1 = styles.add(ParagraphStyle(name='Center',fontName='Vera', alignment=TA_CENTER))
+    p2 = styles.add(ParagraphStyle(name='Right',fontName='Vera', alignment=TA_RIGHT))
+    p3 = styles.add(ParagraphStyle(name='Left',fontName='Vera', alignment=TA_LEFT))
+    p4 = styles.add(ParagraphStyle(name='Line_Data',fontName='Vera', alignment=TA_LEFT, fontSize=11, leading=13))
+    p5 = styles.add(ParagraphStyle(name='Line_Data_Small',fontName='Vera', alignment=TA_LEFT, fontSize=7, leading=8))
+    p6 = styles.add(ParagraphStyle(name='Line_Data_Large',fontName='Vera', alignment=TA_LEFT, fontSize=12, leading=13))
+    p7 = styles.add(ParagraphStyle(name='Line_Data_Largest',fontName='Vera', alignment=TA_LEFT, fontSize=14, leading=15))
+    p8 = styles.add(ParagraphStyle(name='Line_Label',fontName='Vera', font='Helvetica-Bold', fontSize=7, leading=6, alignment=TA_LEFT))
+    p9 = styles.add(ParagraphStyle(name='Line_Label_Center',fontName='Vera', font='Helvetica-Bold', fontSize=7, alignment=TA_CENTER))
+    
 
     ptext = '<font size=12>%s</font>' % formatted_time
     par = Paragraph(ptext, p0)
@@ -158,12 +166,19 @@ def natisni_dobavnica(request,narocilo):
     ]))
     story.append(t1)
     #
-    story.append(Paragraph("NAROČILO", styles["Line_Label_Center"]))
+    story.append(Spacer(1, 20))
+    #
+    story.append(Paragraph("DOBAVNICA", styles["Line_Label_Center"]))
     #
     datum = narocilo.datum.strftime("%d.%m.%Y, %H:%M")
-        
-    data1 = [[Paragraph('DATUM NAROČILA', styles["Line_Label"])],
-            [Paragraph(datum, styles["Line_Data_Largest"])
+    leto_dobavnice = narocilo.datum.strftime("%Y")
+    st_dobavnice = leto_dobavnice[-2:] + str(narocilo.id).zfill(4)
+
+    data1 = [[Paragraph('ŠTEVILKA DOBAVNICE', styles["Line_Label"]),
+              Paragraph('DATUM NAROČILA', styles["Line_Label"])],
+
+            [Paragraph(st_dobavnice, styles["Line_Data_Largest"]),
+             Paragraph(datum, styles["Line_Data_Largest"])
             ]]
     t1 = Table(data1)
     t1.setStyle(TableStyle([
@@ -174,16 +189,47 @@ def natisni_dobavnica(request,narocilo):
     ]))
     story.append(t1)
     #
-    data1 = [[Paragraph('#', styles["Line_Label"]),
-        Paragraph('IME IZDELKA', styles["Line_Label"]),
-        Paragraph('SLIKA', styles["Line_Label"]),
-        Paragraph('KODA IZDELKA', styles["Line_Label"]),
-        Paragraph('VRSTA IZDELKA', styles["Line_Label"]),
-        Paragraph('KOLIČINA', styles["Line_Label"]),
-        Paragraph('✔', styles["Line_Label"])
-        ]]
+
+    data1 = [[Paragraph('NAČIN DOSTAVE', styles["Line_Label"]),
+                Paragraph('NAČIN PRODAJE', styles["Line_Label"])],
+
+                [Paragraph(narocilo.nacin_prodaje, styles["Line_Data_Large"]),
+                Paragraph(narocilo.nacin_dostave, styles["Line_Data_Large"])]
+                ]
+
+    t1 = Table(data1)
+    t1.setStyle(TableStyle([
+        ('INNERGRID', (0, 0), (1, 0), 0.25, colors.black),
+        ('INNERGRID', (0, 1), (1, 1), 0.25, colors.black),
+        ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+    ]))
+    story.append(t1)
+
+    #
+    potnik = narocilo.potnik
+    potnik_podatki = '%s %s <br/> %s <br/> %s ' % (potnik.user.first_name, potnik.user.last_name, potnik.telefon, potnik.email)
+
+    data1 = [[Paragraph('POTNIK', styles["Line_Label"])],
+            [Paragraph(potnik_podatki, styles["Line_Data"])
+            ]]
+    t1 = Table(data1)
+    t1.setStyle(TableStyle([
+        ('INNERGRID', (0, 0), (1, 0), 0.25, colors.black),
+        ('INNERGRID', (0, 1), (1, 1), 0.25, colors.black),
+        ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+    ]))
+    story.append(t1)
+
+    #
+    data1 = [[Paragraph('št', styles["Line_Label"]),
+            Paragraph('črtna koda EAN', styles["Line_Label"]),
+            Paragraph('artikel', styles["Line_Label"]),
+            Paragraph('količina', styles["Line_Label"])
+            ]]
         
-    t1 = Table(data1, colWidths=(1 * cm, 3.6 * cm,5 * cm, 5 * cm, 3 * cm, 1.6 * cm, 0.8 * cm))
+    t1 = Table(data1, colWidths=(1 * cm, 7.7 * cm,6 * cm, 5 * cm))
     t1.setStyle(TableStyle([
         ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
         ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
@@ -191,24 +237,17 @@ def natisni_dobavnica(request,narocilo):
     ]))
     story.append(t1)
     #
-    for i, narocilo_izdelka in enumerate(narocila_izdelka):
+
+    for i, key in enumerate(tabela):
         iteracija = str(i+1) + "."
-        img = utils.ImageReader(narocilo_izdelka.izdelek.image_thumbnail)
-        iw, ih = img.getSize()
-        aspect = ih / float(iw)
-
-        im = Image(narocilo_izdelka.izdelek.image_thumbnail, 2*cm, 2*cm*aspect)
-
+        
         data1 = [[Paragraph(iteracija, styles["Line_Data"]),
-                Paragraph(narocilo_izdelka.izdelek.ime, styles["Line_Data"]),
-                im,
-                Paragraph(narocilo_izdelka.izdelek.koda, styles["Line_Data"]),
-                Paragraph(narocilo_izdelka.izdelek.skupina_izdelkov.ime, styles["Line_Data"]),
-                Paragraph(str(narocilo_izdelka.kolicina), styles["Line_Data"]),
-                Paragraph("", styles["Line_Data"])
+                Paragraph(key.koda, styles["Line_Data"]),
+                Paragraph(key.ime, styles["Line_Data"]),
+                Paragraph(str(tabela[key]), styles["Line_Data"])
                 ]]
 
-        t1 = Table(data1, colWidths=(1 * cm, 3.6 * cm, 5 * cm, 5 * cm, 3 * cm, 1.6 * cm, 0.8 * cm))
+        t1 = Table(data1, colWidths=(1 * cm, 7.7 * cm,6 * cm, 5 * cm))
         t1.setStyle(TableStyle([
             ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
             ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
--- products/naredi_bazo.py
+++ products/naredi_bazo.py
@@ -30,12 +30,30 @@ def naredi_bazo(request):
         user_admin.is_superuser=True
     
     user_admin.save()
-   
+    #naredi potnika
+    user, created = User.objects.get_or_create(username="potnik", email="potnik@gmail.com")
+    user.first_name = "Jure"
+    user.last_name = "Hovelja"
+
+    if created:
+        user.set_password("adminadmin")
+        user.is_staff=True
+        user.is_superuser=False
+    
+    user.save()
+
+    potnik = Potnik(user = user, prodajno_mesto = prodajno_mesto, telefon = "031786453", email = "potnik@gmail.com")
+    potnik.save()
+
+    #
     uporabnik_admin = Uporabnik(user = user_admin, podjetje = podjetje, prodajno_mesto = prodajno_mesto)
     uporabnik_admin.save()
 
-    skupina_izdelkov1 = SkupinaIzdelkov(ime = "Magneti", koda = "238947298347")
+    skupina_izdelkov1 = SkupinaIzdelkov(ime = "Magneti", koda = "2389472983")
     skupina_izdelkov1.save()
+
+    skupina_izdelkov2 = SkupinaIzdelkov(ime = "Razglednice", koda = "2734242398")
+    skupina_izdelkov2.save()
 
     tag_bled = Tag(ime = "Bled")
     tag_bled.save()
@@ -50,8 +68,19 @@ def naredi_bazo(request):
     narocilo_triglav = NarociloIzdelka(izdelek = izdelek_triglav,kolicina = 100)
     narocilo_triglav.save()
 
+    
+
+    izdelek_razglednica1 = Izdelek(ime = "Razglednica test 1", opis = "Dimenzija: 60x80mm", skupina_izdelkov = skupina_izdelkov2, koda = "AZU4as356B")
+    izdelek_razglednica1.slika.save("jama.jpg", File(open(r'media/gallery/mag_panoramski_11.jpg','rb')))
+    narocilo_izdelek_razglednica1 = NarociloIzdelka(izdelek = izdelek_razglednica1,kolicina = 100)
+    narocilo_izdelek_razglednica1.save()
+
+    izdelek_razglednica2 = Izdelek(ime = "Razglednica test 2", opis = "Dimenzija: 60x80mm", skupina_izdelkov = skupina_izdelkov2, koda = "sadfasdf")
+    izdelek_razglednica2.slika.save("marija.jpg", File(open(r'media/gallery/magneti_zbirno_201727.jpg','rb')))
+    narocilo_izdelek_razglednica2 = NarociloIzdelka(izdelek = izdelek_razglednica2,kolicina = 100)
+    narocilo_izdelek_razglednica2.save()
+
     kosarica = Kosarica(uporabnik = uporabnik_admin)
     kosarica.save()
-    kosarica.narocila_izdelka.add(narocilo_bled,narocilo_triglav)
+    kosarica.narocila_izdelka.add(narocilo_bled,narocilo_triglav,narocilo_izdelek_razglednica1,narocilo_izdelek_razglednica2)
     kosarica.save()
-    
