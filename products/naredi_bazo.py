3a895e6c7ed18ddfc853c6b6dc366a486331d9f4
Aljaž <ar1961@student.uni-lj.si>
2018-07-24 19:20:09 +0200

    fix + dodatne slike

--- products/naredi_bazo.py
+++ products/naredi_bazo.py
@@ -66,36 +66,72 @@ def naredi_bazo(request):
     tag_bled.save()
     tag_slovenia = Tag(ime = "Slovenija")
     tag_slovenia.save()
+    tag_bohinj = Tag(ime = "Bohinj")
+    tag_bohinj.save()
+    tag_piran = Tag(ime = "Piran")
+    tag_piran.save()
+    tag_triglav = Tag(ime = "Triglav")
+    tag_triglav.save()
+    tag_jama= Tag(ime = "Postojnska jama")
+    tag_jama.save()
+    tag_lj= Tag(ime = "Ljubljana")
+    tag_lj.save()
+
+    #Bled
+    izdelek_bled = Izdelek(ime = "Magnet Bled", opis = "Dimenzija: 60x80mm", skupina_izdelkov = skupina_izdelkov1, koda = "MAG001")
+    izdelek_bled.slika.save('Bled.jpg', File(open(r'media/gallery/bled.jpg','rb')))
+    izdelek_bled.tag.add(tag_slovenia,tag_bled);
+
+    #narocilo_bled = NarociloIzdelka(izdelek = izdelek_bled,kolicina = 25)
+    #narocilo_bled.save()
+
+    izdelek_triglav = Izdelek(ime = "Magnet Triglav", opis = "Dimenzija: 60x80mm", skupina_izdelkov = skupina_izdelkov1, koda = "MAG015")
+    izdelek_triglav.slika.save("Triglav.jpg", File(open(r'media/gallery/triglav.jpg','rb')))
+    izdelek_triglav.tag.add(tag_slovenia,tag_triglav);
+    #narocilo_triglav = NarociloIzdelka(izdelek = izdelek_triglav,kolicina = 100)
+    #narocilo_triglav.save()
+
     
-    izdelek_bled = Izdelek(ime = "Magnet Bled", opis = "Dimenzija: 60x80mm", skupina_izdelkov = skupina_izdelkov1, koda = "AZU456B")
+
+    izdelek_razglednica1 = Izdelek(ime = "Razglednica Postojnska jama", opis = "Dimenzija: 60x80mm", skupina_izdelkov = skupina_izdelkov2, koda = "RAZ001")
+    izdelek_razglednica1.slika.save("jama.jpg", File(open(r'media/gallery/jama.jpg','rb')))
+    izdelek_razglednica1.tag.add(tag_slovenia,tag_jama)
+    #narocilo_izdelek_razglednica1 = NarociloIzdelka(izdelek = izdelek_razglednica1,kolicina = 100)
+    #narocilo_izdelek_razglednica1.save()
+
+    #izdelek_razglednica2 = Izdelek(ime = "Razglednica test 2", opis = "Dimenzija: 60x80mm", skupina_izdelkov = skupina_izdelkov2, koda = "sadfasdf")
+    #izdelek_razglednica2.slika.save("marija.jpg", File(open(r'media/gallery/magneti_zbirno_201727.jpg','rb')))
+    #narocilo_izdelek_razglednica2 = NarociloIzdelka(izdelek = izdelek_razglednica2,kolicina = 100)
+    #narocilo_izdelek_razglednica2.save()
+
+    #Bohinj
+    izdelek_triglav = Izdelek(ime = "Magnet Bohinj", opis = "Dimenzija: 60x80mm", skupina_izdelkov = skupina_izdelkov1, koda = "MAG003")
+    izdelek_triglav.slika.save("Bohinj.jpg", File(open(r'media/gallery/bohinj.jpg','rb')))
+    izdelek_triglav.tag.add(tag_slovenia,tag_bohinj);
+
+    #Ljubljana
+    izdelek_lj1 = Izdelek(ime = "Magnet Ljubljana 1", opis = "Dimenzija: 60x80mm", skupina_izdelkov = skupina_izdelkov1, koda = "MAG004")
+    izdelek_lj1.slika.save("ljubljana.jpg", File(open(r'media/gallery/ljubljana.jpg','rb')))
+    izdelek_lj1.tag.add(tag_slovenia,tag_lj);
+    #Ljubljana1
+    izdelek_lj2 = Izdelek(ime = "Magnet Ljubljana 2", opis = "Dimenzija: 60x80mm", skupina_izdelkov = skupina_izdelkov1, koda = "MAG005")
+    izdelek_lj2.slika.save("ljubljana1.jpg", File(open(r'media/gallery/ljubljana1.jpg','rb')))
+    izdelek_lj2.tag.add(tag_slovenia,tag_lj);
+    #Ljubljana2
+    izdelek_lj3 = Izdelek(ime = "Magnet Ljubljana 3", opis = "Dimenzija: 60x80mm", skupina_izdelkov = skupina_izdelkov1, koda = "MAG006")
+    izdelek_lj3.slika.save("ljubljana2.jpg", File(open(r'media/gallery/ljubljana2.jpg','rb')))
+    izdelek_lj3.tag.add(tag_slovenia,tag_lj);
     
-    izdelek_bled.slika.save('Bled.jpg', File(open(r'media/gallery/magneti_zbirno_201710.jpg','rb')))
-    izdelek_bled.tag.add(tag_bled);
-    izdelek_bled.tag.add(tag_slovenia);
-
-    narocilo_bled = NarociloIzdelka(izdelek = izdelek_bled,kolicina = 25)
-    narocilo_bled.save()
-
-    izdelek_triglav = Izdelek(ime = "Magnet Triglav", opis = "Dimenzija: 60x80mm", skupina_izdelkov = skupina_izdelkov1, koda = "GHLO99S")
-    izdelek_triglav.slika.save("Triglav.jpg", File(open(r'media/gallery/magneti_zbirno_201711.jpg','rb')))
-    izdelek_triglav.tag.add(tag_slovenia);
+    #Piran
+    izdelek_piran = Izdelek(ime = "Magnet Piran 1", opis = "Dimenzija: 60x80mm", skupina_izdelkov = skupina_izdelkov1, koda = "MAG007")
+    izdelek_piran.slika.save("piran.jpg", File(open(r'media/gallery/piran.jpg','rb')))
+    izdelek_piran.tag.add(tag_slovenia,tag_piran);
+    #Piran1
+    izdelek_piran1 = Izdelek(ime = "Magnet Piran 2 ", opis = "Dimenzija: 60x80mm", skupina_izdelkov = skupina_izdelkov1, koda = "MAG008")
+    izdelek_piran1.slika.save("piran1.jpg", File(open(r'media/gallery/piran1.jpg','rb')))
+    izdelek_piran1.tag.add(tag_slovenia,tag_piran);
     
-    narocilo_triglav = NarociloIzdelka(izdelek = izdelek_triglav,kolicina = 100)
-    narocilo_triglav.save()
-
-    
-
-    izdelek_razglednica1 = Izdelek(ime = "Razglednica test 1", opis = "Dimenzija: 60x80mm", skupina_izdelkov = skupina_izdelkov2, koda = "AZU4as356B")
-    izdelek_razglednica1.slika.save("jama.jpg", File(open(r'media/gallery/mag_panoramski_11.jpg','rb')))
-    narocilo_izdelek_razglednica1 = NarociloIzdelka(izdelek = izdelek_razglednica1,kolicina = 100)
-    narocilo_izdelek_razglednica1.save()
-
-    izdelek_razglednica2 = Izdelek(ime = "Razglednica test 2", opis = "Dimenzija: 60x80mm", skupina_izdelkov = skupina_izdelkov2, koda = "sadfasdf")
-    izdelek_razglednica2.slika.save("marija.jpg", File(open(r'media/gallery/magneti_zbirno_201727.jpg','rb')))
-    narocilo_izdelek_razglednica2 = NarociloIzdelka(izdelek = izdelek_razglednica2,kolicina = 100)
-    narocilo_izdelek_razglednica2.save()
-
-    kosarica = Kosarica(uporabnik = uporabnik)
-    kosarica.save()
-    kosarica.narocila_izdelka.add(narocilo_bled,narocilo_triglav,narocilo_izdelek_razglednica1,narocilo_izdelek_razglednica2)
-    kosarica.save()
+    #kosarica = Kosarica(uporabnik = uporabnik)
+    #kosarica.save()
+    #kosarica.narocila_izdelka.add(narocilo_bled,narocilo_triglav,narocilo_izdelek_razglednica1,narocilo_izdelek_razglednica2)
+    #kosarica.save()
