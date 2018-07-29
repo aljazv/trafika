25776bfc2a3e9f1130866852ce7ce2659b05b04e
Unknown <mo2625@student.uni-lj.si>
2018-07-29 12:02:26 +0200

    dodan stevilo prikazov na stran

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
 	path('kosarica/', views.kosarica, name='kosarica'),
 	path('pregled_narocil/', views.pregled_narocil, name='pregled_narocil'),
     path('<int:index>/',views.index_skupina,name='index_skupina'),
-    path('<int:index>/<str:search_string>',views.search,name='search')
+    path('<int:index>/<str:search_string>',views.search,name='search'),
+    path('session/',views.save_session,name='save_session')
 ]
