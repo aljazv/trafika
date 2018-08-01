47db77fa2ea479c7b88c7aa4c8acc6ce5043eb7c
Unknown <aljaz.verlic@gmail.com>
2018-08-01 19:29:56 +0200

    spreminjanje narocila admin page customize

--- products/admin.py
+++ products/admin.py
@@ -5,14 +5,43 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Izdelek)
+
+
+class IzdelekAdmin(admin.ModelAdmin):
+    list_display = ('ime','ean_koda','koda')
+    search_fields = ['id','ean_koda','koda']
+
+class NarociloIzdelkaAdmin(admin.ModelAdmin):
+    list_display = ('__str__','id','izdelek')
+    search_fields = ['id']
+
+class ProdajnoMestoAdmin(admin.ModelAdmin):
+    list_display = ('ime','naslov','potnik')
+
+class PodjetjeAdmin(admin.ModelAdmin):
+    list_display = ('ime','naslov')
+class UporabnikAdmin(admin.ModelAdmin):
+    list_display = ('__str__','podjetje','prodajno_mesto')
+class PotnikAdmin(admin.ModelAdmin):
+    list_display = ('__str__','telefon','email')
+
+class NarociloAdmin(admin.ModelAdmin):
+    list_display = ('__str__','id','datum','st_narocila')
+    list_filter = ['datum']
+    search_fields = ['id','st_narocila','datum']
+
+
+
+
+
+admin.site.register(Izdelek,IzdelekAdmin)
 admin.site.register(SkupinaIzdelkov)
 admin.site.register(Tag)
-admin.site.register(Uporabnik)
-admin.site.register(Narocilo)
-admin.site.register(NarociloIzdelka)
+admin.site.register(Uporabnik,UporabnikAdmin)
+admin.site.register(Narocilo,NarociloAdmin)
+admin.site.register(NarociloIzdelka,NarociloIzdelkaAdmin)
 admin.site.register(Kosarica)
-admin.site.register(Potnik)
-admin.site.register(ProdajnoMesto)
-admin.site.register(Podjetje)
+admin.site.register(Potnik,PotnikAdmin)
+admin.site.register(ProdajnoMesto,ProdajnoMestoAdmin)
+admin.site.register(Podjetje,PodjetjeAdmin)
 
