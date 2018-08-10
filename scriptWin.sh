aa63d011aa8c8f72a712179c3d1b87dd4c8cd9c1
Unknown <mo2625@student.uni-lj.si>
2018-08-10 13:57:38 +0200

    commit pred deployem p.2

--- scriptWin.sh
+++
@@ -1,9 +0,0 @@
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc"  -delete
-
-rm db.sqlite3
-
-python manage.py makemigrations
-python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminadmin', first_name='Skrbnik')" | python manage.py shell
-echo "import products.naredi_bazo; products.naredi_bazo.naredi_bazo(None)" | python manage.py shell
